37053543c0b2b7f828f74f8f72d89c5a20c8c44b
Glimsura <antihete12@gmail.com>
2024-12-20 21:38:17 +0200

    Lab 4 - add JWTManager and JWT error handling in __init__.py, JWT_SECRET_KEY in config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,32 @@
-from flask import Flask
-from app.extensions import db, migrate
-from app.models import UserModel, CategoryModel, ExpenseModel
-from app.routes.categories import bp as categories_bp
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+
+from app.models import db
+
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
-    db.init_app(app)
-    migrate.init_app(app, db)
+    app.config.from_pyfile('config.py', silent=True)
 
-    # Реєстрація Blueprint
-    app.register_blueprint(categories_bp)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    jwt = JWTManager(app)
+
+    from app.views import views
+    app.register_blueprint(views)
+
+    # JWT error handling
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return jsonify({"message": "The token has expired.", "error": "token_expired"}), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message": "Invalid token.", "error": "invalid_token"}), 401
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({"message": "Missing access token.", "error": "authorization_required"}), 401
 
     return app
--- app/config.py
+++ app/config.py
@@ -2,14 +2,8 @@ import os
 
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
-SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ.get('POSTGRES_USER', 'user')}:" \
-                          f"{os.environ.get('POSTGRES_PASSWORD', 'mysecurepassword')}@" \
-                          f"{os.environ.get('DB_HOST', 'localhost')}/" \
-                          f"{os.environ.get('POSTGRES_DB', 'finance_app')}"
+SQLALCHEMY_DATABASE_URI = f'postgresql://{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["DB_HOST"]}/{os.environ["POSTGRES_DB"]}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 API_TITLE = "Finance REST API"
 API_VERSION = "v1"
-OPENAPI_VERSION = "3.0.3"
-OPENAPI_URL_PREFIX = "/"
-OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
-OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
