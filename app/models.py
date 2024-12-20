aab4ca2166011de9e6ec1e65a0a0300d92adb7fc
Glimsura <antihete12@gmail.com>
2024-12-20 21:39:21 +0200

    Lab 4 - add UserModel, update UserSchema

--- app/models.py
+++ app/models.py
@@ -2,34 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class UserModel(db.Model):
-    __tablename__ = "users"
+class Accounts(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, nullable=False, unique=True)
+    balance = db.Column(db.Float, default=0.0, nullable=False)
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), nullable=False, unique=True)
-    default_currency = db.Column(db.String(3), nullable=False, default="USD")
-    categories = db.relationship("CategoryModel", back_populates="user", lazy="dynamic")
-    expenses = db.relationship("ExpenseModel", back_populates="user", lazy="dynamic")
-
-
-class CategoryModel(db.Model):
-    __tablename__ = "categories"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    is_global = db.Column(db.Boolean, default=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
-    user = db.relationship("UserModel", back_populates="categories")
-
-
-class ExpenseModel(db.Model):
-    __tablename__ = "expenses"
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
-    amount = db.Column(db.Float, nullable=False)
-    currency = db.Column(db.String(3), nullable=False)
-    created_at = db.Column(db.TIMESTAMP, server_default=db.func.now())
-    user = db.relationship("UserModel", back_populates="expenses")
-    category = db.relationship("CategoryModel", back_populates="categories")
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
